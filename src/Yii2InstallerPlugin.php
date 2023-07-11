1901980288c1a79768ffd64e88bcf32dbc333da0
João Marques <joao@jjmf.com>
2023-07-11 15:00:24 -0300

    Create Yii2InstallerPlugin.php

---
+++ src/Yii2InstallerPlugin.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace marqu3s\Composer;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class Yii2InstallerPlugin implements PluginInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $installer = new TemplateInstaller($io, $composer);
+        $composer->getInstallationManager()->addInstaller($installer);
+    }
+}
